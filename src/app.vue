ab86bde04c7e035c25537fe893d3ed171a31bd02
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2018-10-11 19:28:11 -0500

    Add bulma menu for words

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,3 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Words</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
--- src/components/entry_list.vue
+++ src/components/entry_list.vue
@@ -1,22 +1,73 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="e in entries"> {{e.entry}} ({{e.translation}})</li>
-    </ul>
+  <div class="columns">
+    <div class="column is-one-third">
+      <nav class="panel">
+        <p class="panel-heading"> Words (Georgian)</p>
+        <div class="panel-block">
+          <p class="control has-icons-left">
+            <input class="input" type="text" placeholder="search">
+          </p>
+        </div>
+        <p class="panel-tabs">
+          <a class="is-active">all</a>
+          <a>verbs</a>
+          <a>nounts</a>
+          <a>adverbs</a>
+        </p>
+        <a class="panel-block" v-for="e in entries">
+          <b>{{e.entry }}</b> &nbsp;- {{e.translation}}
+        </a>
+      </nav>
+    </div>
+    <div class="column">
+      <div class="card is-hidden-mobile">
+        <header class="card-header">
+          <p class="card-header-title">
+            Component
+          </p>
+          <a href="#" class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">Save</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+
   </div>
+
 </template>
 <script>
 export default {
   data: function() {
     return {
       entries: []
-    }
+    };
   },
-  created: function(){
-    this.$http.get('https://api.neolang.app/v1/entries')
-      .then(function(res){
-        this.entries = res.data;
-      });
+  created: function() {
+    this.$http.get("https://api.neolang.app/v1/entries").then(function(res) {
+      this.entries = res.data;
+    });
   }
 };
 </script>
+<style>
+  #words {
+   position: fixed;
+   top: 1rem;
+   bottom: 1rem;
+ }
+</style>
