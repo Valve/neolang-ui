a0a64a62e7e2a2882ea5c36f7bdb5f82f99f2a00
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2018-10-12 18:50:17 -0500

    Implement card showing and hiding

--- src/views/entries.vue
+++ src/views/entries.vue
@@ -8,16 +8,21 @@
       </nav>
     </header>
     <div class="row">
-      <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 entries-container">
-        <entry_list v-on:click="entryClicked"/>
-      </div>
-      <div class="col-lg-8 entries-container">
-        <entry_card v-bind:entry="entry"/>
+      <div class="col entries-container">
+        <entry_list v-on:click="entryClick" v-bind:visible="!entry.selected"/>
+        <entry_card v-bind:entry="entry" v-bind:visible="entry.selected" v-on:close="cardClose"/>
       </div>
     </div>
   </div>
 </template>
 <script>
+const defaultEntry = {
+  id: 0, 
+  entry: "Hi!", 
+  translation: "გამარჯობა!", 
+  comment: "Click on a word to get started",
+  selected: false
+};
 import entry_list from '@/components/entry_list.vue';
 import entry_card from '@/components/entry_card.vue';
 export default {
@@ -27,12 +32,16 @@ export default {
   },
   data: function() {
     return {
-      entry: {id: 0, entry: "Hi!", translation: "გამარჯობა!", comment: "Click on a word on the left to get started"}
+      entry: defaultEntry
     }
   },
   methods: {
-    entryClicked: function(entry) {
+    entryClick: function(entry) {
       this.entry = entry;
+      this.entry.selected = true;
+    },
+    cardClose: function() {
+      this.entry = defaultEntry;
     }
   }
 };
